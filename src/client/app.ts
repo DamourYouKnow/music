bb11e474a624fcd272b48ae16e0185fc52b0dc6d
damouryouknow <bailey@damour.xyz>
2020-11-14 19:39:51 -0500

    Add really simple queue list interface

--- src/client/app.ts
+++ src/client/app.ts
@@ -7,6 +7,18 @@ if (!audioStream) throw Error('No audio stream element');
 const eventSource = new EventSource('/join/a');
 eventSource.addEventListener('message', (message) => {
     const room = JSON.parse(message.data);
+
+    // Update queue interface.
+    const queue = document.getElementById('queue');
+    if (!queue) throw Error('No queue element');
+    queue.innerHTML = '';
+    for (const content of room.queue) {
+        const item = document.createElement('li');
+        const duration = timeStr(new Date(content.length * 1000));
+        item.textContent = `${duration} - ${content.title}`;
+        queue.appendChild(item);
+    }
+
     const track = room.queue[0];
     if (track) {
         audioStream.src = `/track/${track.id}.mp3`;
@@ -26,3 +38,10 @@ queueBtn.onclick = async () => {
     };
     await axios.post('/queue', data);
 };
+
+
+function timeStr(time: Date): string {
+    const minutes = time.getMinutes().toString().padStart(2, '0');
+    const seconds = time.getSeconds().toString().padStart(2, '0');
+    return `${minutes}:${seconds}`;
+}
